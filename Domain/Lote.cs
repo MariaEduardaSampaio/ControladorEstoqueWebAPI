45a880fa574a069cec6aadbd8337435f9f2d0681
Maria Eduarda Sampaio <dudah.s3@gmail.com>
2024-02-28 23:50:59 -0300

    Add project files.

---
+++ Domain/Lote.cs
@@ -0,0 +1,9 @@
+﻿namespace Domain
+{
+    public class Lote
+    {
+        public int Id { get; set; }
+        public List<Produto> Produtos { get; set; }
+        public string Codigo { get; set; }
+    }
+}
---
+++ Domain/Produto.cs
@@ -0,0 +1,11 @@
+﻿namespace Domain
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public List<Item> Itens { get; set; }
+        public Lote Lote { get; set; } 
+        public int LoteId { get; set; }
+    }
+}
