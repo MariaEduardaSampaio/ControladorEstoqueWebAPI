075207f585adaa6202b55df217f4a1f7acaf601f
Maria Eduarda Sampaio <dudah.s3@gmail.com>
2024-03-02 18:57:06 -0300

    fix: adiciona data annotation para ignorar prop lotes e nao causar ref ciclica na impressao pelo swagger

--- Domain/Entities/Produto.cs
+++ Domain/Entities/Produto.cs
@@ -1,9 +1,13 @@
-﻿namespace Domain.Entities
+﻿using System.Text.Json.Serialization;
+
+namespace Domain.Entities
 {
     public class Produto
     {
         public int Id { get; set; }
         public string Nome { get; set; }
+
+        [JsonIgnore]
         public List<Lote> Lotes { get; set; }
     }
 }
