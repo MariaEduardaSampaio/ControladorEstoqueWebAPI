877b012ea2f08421022ccb36ad9feb5c185a28a6
Maria Eduarda Sampaio <dudah.s3@gmail.com>
2024-03-02 19:01:42 -0300

    add: cria object result customizado para retorno das rotas

---
+++ ControladorEstoqueWebAPI/CustomObjectResult.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+public class CustomObjectResult : IActionResult
+{
+    public string Message { get; set; }
+    public int StatusCode { get; set; }
+    public object? Object { get; set; }
+
+    public async Task ExecuteResultAsync(ActionContext context)
+    {
+        var response = context.HttpContext.Response;
+        response.StatusCode = StatusCode;
+
+        var options = new JsonSerializerOptions
+        {
+            ReferenceHandler = ReferenceHandler.Preserve,
+        };
+
+        if (Object != null)
+            await response.WriteAsJsonAsync(Object, options);
+        else
+            await response.WriteAsync(Message);
+    }
+}
