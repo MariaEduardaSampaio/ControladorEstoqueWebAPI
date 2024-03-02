e15f8d8e4ef1b8f84daf9e9d83d9ea8c5807925b
Maria Eduarda Sampaio <dudah.s3@gmail.com>
2024-03-01 22:02:10 -0300

    add: cria contexto do banco de dados sql server

---
+++ Infrastructure/Context/ControladorEstoqueContext.cs
@@ -0,0 +1,22 @@
+﻿using Domain.Entities;
+using Infrastructure.EntityFramework.Configuration;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Context
+{
+    public class ControladorEstoqueContext : DbContext
+    {
+        public DbSet<Lote> Lotes { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=ControladorEstoque;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new LoteConfiguration());
+            modelBuilder.ApplyConfiguration(new ProdutoConfiguration());
+        }
+    }
+}
