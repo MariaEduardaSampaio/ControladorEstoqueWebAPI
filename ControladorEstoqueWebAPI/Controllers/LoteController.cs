2d417fe50f0fdc5c04fe2b148a470a68b5d7e313
Maria Eduarda Sampaio <dudah.s3@gmail.com>
2024-03-02 19:00:55 -0300

    add: adiciona controllers

---
+++ ControladorEstoqueWebAPI/Controllers/LoteController.cs
@@ -0,0 +1,227 @@
+﻿using Domain.Entities;
+using Domain.Interfaces.Services;
+using Domain.Requests;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace Application.Controllers
+{
+    [ApiController]
+    [Route("[controller]/")]
+    public class LoteController
+    {
+        private readonly ILoteService _service;
+
+        public LoteController(ILoteService service)
+        {
+            _service = service;
+        }
+
+        [HttpPost("AdicionarLote", Name = "Adicionar lote")]
+        public IActionResult AdicionarLote([FromBody] LoteRequest request)
+        {
+            try
+            {
+                var lote = _service.AdicionarLote(request);
+                return new CustomObjectResult()
+                {
+                    Object = lote,
+                    Message = "Lote criado com sucesso!",
+                    StatusCode = (int)HttpStatusCode.Created
+                };
+            }
+            catch (Exception ex)
+            {
+                return new CustomObjectResult()
+                {
+                    Message = $"Não foi possível criar um lote: {ex.Message}",
+                    StatusCode = (int)HttpStatusCode.Created
+                };
+            }
+        }
+
+        [HttpGet("ListarTodosOsLotes", Name = "Listar todos os lotes")]
+        public IActionResult ListarTodosOsLotes()
+        {
+            try
+            {
+                var lotes = _service.ListarTodosOsLotes();
+                return new CustomObjectResult()
+                {
+                    Object = lotes,
+                    Message = "Todos os lotes registrados.",
+                    StatusCode = (int)HttpStatusCode.OK
+                };
+            }
+            catch (Exception ex)
+            {
+                return new CustomObjectResult()
+                {
+                    Message = $"Não foi possível listar todos os lotes: {ex.Message}",
+                    StatusCode = (int)HttpStatusCode.BadRequest
+                };
+            }
+        }
+
+        [HttpGet("ListarLotesPorProduto", Name = "Listar todos os lotes de um produto")]
+        public IActionResult ListarLotesPorProduto([FromQuery] int idProduto)
+        {
+            try
+            {
+                var lotes = _service.ListarLotesPorProduto(idProduto);
+                return new CustomObjectResult()
+                {
+                    Object = lotes,
+                    Message = $"Todos os lotes do produto de ID {idProduto}.",
+                    StatusCode = (int)HttpStatusCode.OK
+                };
+            }
+            catch (Exception ex)
+            {
+                return new CustomObjectResult()
+                {
+                    Message = $"Não foi possível listar todos os lotes do produto {idProduto}: {ex.Message}",
+                    StatusCode = (int)HttpStatusCode.BadRequest
+                };
+            }
+        }
+
+        [HttpGet("ListarLotesVencidos", Name = "Listar todos os lotes vencidos")]
+        public IActionResult ListarLotesVencidos()
+        {
+            try
+            {
+                var lotes = _service.ListarLotesVencidos();
+                return new CustomObjectResult()
+                {
+                    Object = lotes,
+                    Message = "Todos os lotes vencidos.",
+                    StatusCode = (int)HttpStatusCode.OK
+                };
+            }
+            catch (Exception ex)
+            {
+                return new CustomObjectResult()
+                {
+                    Message = $"Não foi possível listar todos os lotes vencidos do sistema: {ex.Message}",
+                    StatusCode = (int)HttpStatusCode.BadRequest
+                };
+            }
+        }
+
+        [HttpGet("ListarLotePorID", Name = "Listar lote por ID")]
+        public IActionResult ListarLotePorID([FromQuery] int id)
+        {
+            try
+            {
+                var lote = _service.ListarLotePorID(id);
+                return new CustomObjectResult()
+                {
+                    Object = lote,
+                    Message = $"Lote de id {id} retornado com sucesso.",
+                    StatusCode = (int)HttpStatusCode.OK
+                };
+            }
+            catch (Exception ex)
+            {
+                return new CustomObjectResult()
+                {
+                    Message = $"Não foi possível encontrar um lote com ID {id}: {ex.Message}",
+                    StatusCode = (int)HttpStatusCode.BadRequest
+                };
+            }
+        }
+
+        [HttpGet("ListarLotePorCodigo", Name = "Listar lote por código")]
+        public IActionResult ListarLotePorCodigo([FromQuery] string codigo)
+        {
+            try
+            {
+                var lote = _service.ListarLotePorCodigo(codigo);
+                return new CustomObjectResult()
+                {
+                    Object = lote,
+                    Message = $"Lote de código {codigo} retornado com sucesso.",
+                    StatusCode = (int)HttpStatusCode.OK
+                };
+            }
+            catch (Exception ex)
+            {
+                return new CustomObjectResult()
+                {
+                    Message = $"Não foi possível encontrar um lote com código {codigo}: {ex.Message}",
+                    StatusCode = (int)HttpStatusCode.BadRequest
+                };
+            }
+        }
+
+        [HttpPut("AtualizarLote", Name = "Atualizar lote")]
+        public IActionResult AtualizarLote([FromBody] LoteRequest request, [FromQuery] int id)
+        {
+            try
+            {
+                var lote = _service.AtualizarLote(request, id);
+                return new CustomObjectResult()
+                {
+                    Object = lote,
+                    Message = $"Alterações salvas com sucesso!",
+                    StatusCode = (int)HttpStatusCode.OK
+                };
+            }
+            catch (Exception ex)
+            {
+                return new CustomObjectResult()
+                {
+                    Message = $"Não foi possível atualizar o lote: {ex.Message}",
+                    StatusCode = (int)HttpStatusCode.BadRequest
+                };
+            }
+        }
+
+        [HttpDelete("RemoverLote", Name = "Remover lote")]
+        public IActionResult RemoverLote([FromQuery] int id)
+        {
+            try
+            {
+                var lote = _service.RemoverLote(id);
+                return new CustomObjectResult()
+                {
+                    Object = lote,
+                    Message = $"Lote removido com sucesso!",
+                    StatusCode = (int)HttpStatusCode.OK
+                };
+            }
+            catch (Exception ex)
+            {
+                return new CustomObjectResult()
+                {
+                    Message = $"Não foi possível remover o lote: {ex.Message}",
+                    StatusCode = (int)HttpStatusCode.BadRequest
+                };
+            }
+        }
+
+        [HttpDelete("RemoverLotesVencidos", Name = "Remover lotes vencidos")]
+        public IActionResult RemoverLotesVencidos()
+        {
+            try
+            {
+                var lotes = _service.RemoverLotesVencidos();
+                return new CustomObjectResult()
+                {
+                    Object = lotes,
+                    Message = $"Lotes vencidos removidos com sucesso!",
+                    StatusCode = (int)HttpStatusCode.OK
+                };
+            }
+            catch (Exception ex)
+            {
+                return new CustomObjectResult()
+                {
+                    Message = $"Não foi possível remover os lotes vencidos: {ex.Message}",
+                    StatusCode = (int)HttpStatusCode.BadRequest
+                };
+            }
+        }
+    }
+}
---
+++ ControladorEstoqueWebAPI/Controllers/ProdutoController.cs
@@ -0,0 +1,159 @@
+﻿using Azure.Core;
+using Domain.Entities;
+using Domain.Interfaces.Services;
+using Domain.Requests;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace Application.Controllers
+{
+    [ApiController]
+    [Route("[controller]/")]
+    public class ProdutoController : Controller
+    {
+        private readonly IProdutoService _service;
+
+        public ProdutoController(IProdutoService service)
+        {
+            _service = service;
+        }
+
+        [HttpPost("AdicionarProduto", Name = "Adicionar produto")]
+        public IActionResult AdicionarProduto([FromBody] ProdutoRequest request)
+        {
+            try
+            {
+                var produto = _service.AdicionarProduto(request);
+                return new CustomObjectResult()
+                {
+                    Object = produto,
+                    Message = "Produto criado com sucesso!",
+                    StatusCode = (int)HttpStatusCode.Created
+                };
+            }
+            catch (Exception ex)
+            {
+                return new CustomObjectResult()
+                {
+                    Message = $"Não foi possível adicionar um produto: {ex.Message}",
+                    StatusCode = (int)HttpStatusCode.BadRequest
+                };
+            }
+        }
+
+        [HttpGet("ListarTodosOsProdutos", Name = "Listar todos os produtos")]
+        public IActionResult ListarTodosOsProdutos()
+        {
+            try
+            {
+                var produtos = _service.ListarTodosOsProdutos();
+                return new CustomObjectResult()
+                {
+                    Object = produtos,
+                    Message = "Todos os produtos registrados.",
+                    StatusCode = (int)HttpStatusCode.OK
+                };
+            }
+            catch (Exception ex)
+            {
+                return new CustomObjectResult()
+                {
+                    Message = $"Não foi possível listar todos os produtos: {ex.Message}",
+                    StatusCode = (int)HttpStatusCode.BadRequest
+                };
+            }
+        }
+
+        [HttpGet("ListarProdutosPorNome", Name = "Listar produtos por nome")]
+        public IActionResult ListarProdutosPorNome([FromQuery] string nome)
+        {
+            try
+            {
+                var produtos = _service.ListarProdutosPorNome(nome);
+                return new CustomObjectResult()
+                {
+                    Object = produtos,
+                    Message = $"Todos os produtos de nome {nome}.",
+                    StatusCode = (int)HttpStatusCode.OK
+                };
+            }
+            catch (Exception ex)
+            {
+                return new CustomObjectResult()
+                {
+                    Message = $"Não foi possível listar os produtos com nome {nome}: {ex.Message}",
+                    StatusCode = (int)HttpStatusCode.BadRequest
+                };
+            }
+        }
+
+        [HttpGet("ListarProdutoPorID", Name = "Listar produto por ID")]
+        public IActionResult ListarProdutoPorID([FromQuery] int id)
+        {
+            try
+            {
+                var produto = _service.ListarProdutoPorID(id);
+                return new CustomObjectResult()
+                {
+                    Object = produto,
+                    Message = $"Produto de id {id} retornado com sucesso.",
+                    StatusCode = (int)HttpStatusCode.OK
+                };
+            }
+            catch (Exception ex)
+            {
+                return new CustomObjectResult()
+                {
+                    Message = $"Não foi possível encontrar um produto com ID {id}: {ex.Message}",
+                    StatusCode = (int)HttpStatusCode.BadRequest
+                };
+            }
+        }
+
+        [HttpPut("AtualizarProduto", Name = "Atualizar produto")]
+        public IActionResult AtualizarProduto([FromBody] ProdutoRequest request, [FromQuery] int id)
+        {
+            try
+            {
+                var produto = _service.AtualizarProduto(request, id);
+                return new CustomObjectResult()
+                {
+                    Object = produto,
+                    Message = $"Alterações salvas com sucesso!",
+                    StatusCode = (int)HttpStatusCode.OK
+                };
+            }
+            catch (Exception ex)
+            {
+                return new CustomObjectResult()
+                {
+                    Message = $"Não foi possível atualizar o produto: {ex.Message}",
+                    StatusCode = (int)HttpStatusCode.BadRequest
+                };
+            }
+        }
+
+        [HttpDelete("RemoverProduto", Name = "Remover produto")]
+        public IActionResult RemoverProduto([FromQuery] int id)
+        {
+            try
+            {
+                var produto = _service.RemoverProduto(id);
+                return new CustomObjectResult()
+                {
+                    Object = produto,
+                    Message = $"Produto removido com sucesso!",
+                    StatusCode = (int)HttpStatusCode.OK
+                };
+            }
+            catch (Exception ex)
+            {
+                return new CustomObjectResult()
+                {
+                    Message = $"Não foi possível remover o produto: {ex.Message}",
+                    StatusCode = (int)HttpStatusCode.BadRequest
+                };
+            }
+        }
+    }
+}
