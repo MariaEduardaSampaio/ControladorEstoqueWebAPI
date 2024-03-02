422dfa3df5f19f9b2f9115583ec96afabf3d251f
Maria Eduarda Sampaio <dudah.s3@gmail.com>
2024-03-01 22:03:31 -0300

    feat: implementa interfaces de repositórios

---
+++ Infrastructure/Repository/LoteRepository.cs
@@ -0,0 +1,71 @@
+﻿using Domain.Entities;
+using Domain.Interfaces.Repositories;
+using Infrastructure.Context;
+
+namespace Infrastructure.Repository
+{
+    public class LoteRepository : ILoteRepository
+    {
+        protected readonly ControladorEstoqueContext _controladorEstoqueContext;
+
+        public LoteRepository(ControladorEstoqueContext controladorEstoqueContext)
+        {
+            _controladorEstoqueContext = controladorEstoqueContext;
+        }
+
+        public int Create(Lote lote)
+        {
+            _controladorEstoqueContext.Lotes.Add(lote);
+            SaveChanges();
+            return lote.Id;
+        }
+
+        public Lote? Delete(int id)
+        {
+            var lote = _controladorEstoqueContext.Lotes.Find(id);
+
+            if (lote is not null)
+                _controladorEstoqueContext.Lotes.Remove(lote);
+
+            SaveChanges();
+
+            return lote;
+        }
+
+        public IEnumerable<Lote> GetAll()
+        {
+            return _controladorEstoqueContext.Lotes.ToList();
+        }
+
+        public IEnumerable<Lote>? GetAllByProduct(int idProduct)
+        {
+            IEnumerable<Lote> lotesDeProduto = [];
+            var produto = _controladorEstoqueContext.Produtos.Find(idProduct);
+
+            if (produto is not null)
+                lotesDeProduto = _controladorEstoqueContext.Lotes.Where(l => l.Produto == produto).ToList();
+            
+            return lotesDeProduto;
+        }
+
+        public Lote? GetByCode(string code)
+        {
+            return _controladorEstoqueContext.Lotes.FirstOrDefault(l => l.Codigo == code);
+        }
+
+        public Lote? GetByID(int id)
+        {
+            return _controladorEstoqueContext.Lotes.Find(id);
+        }
+
+        public void Update(Lote lote)
+        {
+            _controladorEstoqueContext.Update(lote);
+            SaveChanges();
+        }
+        private void SaveChanges()
+        {
+            _controladorEstoqueContext.SaveChanges();
+        }
+    }
+}
---
+++ Infrastructure/Repository/ProdutoRepository.cs
@@ -0,0 +1,60 @@
+﻿using Domain.Entities;
+using Domain.Interfaces.Repositories;
+using Infrastructure.Context;
+
+namespace Infrastructure.Repository
+{
+    internal class ProdutoRepository : IProdutoRepository
+    {
+        protected readonly ControladorEstoqueContext _controladorEstoqueContext;
+
+        public ProdutoRepository(ControladorEstoqueContext controladorEstoqueContext)
+        {
+            _controladorEstoqueContext = controladorEstoqueContext;
+        }
+        public int Create(Produto produto)
+        {
+            _controladorEstoqueContext.Produtos.Add(produto);
+            SaveChanges();
+            return produto.Id;
+        }
+
+        public Produto? Delete(int id)
+        {
+            var produto = _controladorEstoqueContext.Produtos.Find(id);
+
+            if (produto is not null)
+                _controladorEstoqueContext.Produtos.Remove(produto);
+
+            SaveChanges();
+
+            return produto;
+        }
+
+        public IEnumerable<Produto> GetAll()
+        {
+            return _controladorEstoqueContext.Produtos.ToList();
+        }
+
+        public Produto? GetByID(int id)
+        {
+            return _controladorEstoqueContext.Produtos.Find(id);
+        }
+
+        public Produto? GetByName(string name)
+        {
+            return _controladorEstoqueContext.Produtos.FirstOrDefault(p => p.Nome == name);
+        }
+
+        public void Update(Produto produto)
+        {
+            _controladorEstoqueContext.Produtos.Update(produto);
+            SaveChanges();
+        }
+
+        private void SaveChanges()
+        {
+            _controladorEstoqueContext.SaveChanges();
+        }
+    }
+}
