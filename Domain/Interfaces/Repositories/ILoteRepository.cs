73dd7a73e6689695f0111a8642a6c48069037061
Maria Eduarda Sampaio <dudah.s3@gmail.com>
2024-03-01 22:01:21 -0300

    add: cria interfaces para repositórios de cada entidade

---
+++ Domain/Interfaces/Repositories/ILoteRepository.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Entities;
+
+namespace Domain.Interfaces.Repositories
+{
+    public interface ILoteRepository
+    {
+        int Create(Lote lote);
+        IEnumerable<Lote> GetAll();
+        IEnumerable<Lote> GetAllByProduct(int idProduct);
+        Lote? GetByID(int id);
+        Lote? GetByCode(string code);
+        void Update(Lote lote);
+        Lote? Delete(int id);
+    }
+}
---
+++ Domain/Interfaces/Repositories/IProdutoRepository.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Entities;
+
+namespace Domain.Interfaces.Repositories
+{
+    public interface IProdutoRepository
+    {
+        int Create(Produto produto);
+        IEnumerable<Produto> GetAll();
+        Produto? GetByID(int id);
+        Produto? GetByName(string name);
+        void Update(Produto produto);
+        Produto? Delete(int id);
+    }
+}
