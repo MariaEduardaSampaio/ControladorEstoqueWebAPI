1915328dff1c1054b485a7c3b1e8aa5e0aacd459
Maria Eduarda Sampaio <dudah.s3@gmail.com>
2024-03-02 18:58:49 -0300

    feat: adiciona data annotation p nome ter no mínimo tamanho de 2 caracteres

--- Domain/Requests/ProdutoRequest.cs
+++ Domain/Requests/ProdutoRequest.cs
@@ -1,15 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace Domain.Requests
 {
     public class ProdutoRequest
     {
         [Required]
+        [MinLength(2)]
         public string Nome { get; set; }
     }
 }
