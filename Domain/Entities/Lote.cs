9b4dd046705f45b418a7ae07e78d5ef444f73e60
Maria Eduarda Sampaio <dudah.s3@gmail.com>
2024-03-01 22:00:51 -0300

    fix: retira propriedades desnecessarias e corrige entidade p atender requisitos

---
+++ Domain/Entities/Lote.cs
@@ -0,0 +1,13 @@
+﻿namespace Domain.Entities
+{
+    public class Lote
+    {
+        public int Id { get; set; }
+        public string Codigo { get; set; }
+        public int Quantidade { get; set; }
+        public DateOnly Fabricacao { get; set; }
+        public DateOnly Validade { get; set; }
+        public Produto Produto { get; set; }
+        public int ProdutoId { get; set; }
+    }
+}
