9ec301a0b78be068f3c3cd210334f6d0c5a89519
Maria Eduarda Sampaio <dudah.s3@gmail.com>
2024-03-01 22:02:53 -0300

    add: cria configuração / mapeamento das propriedades p tabelas do banco

---
+++ Infrastructure/EntityFramework/Configuration/LoteConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.EntityFramework.Configuration
+{
+    public class LoteConfiguration : IEntityTypeConfiguration<Lote>
+    {
+        public void Configure(EntityTypeBuilder<Lote> builder)
+        {
+            builder.HasKey(l => l.Id);
+            builder.Property(l => l.Id).ValueGeneratedOnAdd().UseIdentityColumn();
+            builder.Property(l => l.Codigo).IsRequired();
+            builder.Property(l => l.Quantidade).IsRequired();
+            builder.Property(l => l.Fabricacao);
+            builder.Property(l => l.Validade).IsRequired();
+            builder.HasOne(l => l.Produto).WithMany(p => p.Lotes).HasForeignKey(l => l.ProdutoId);
+        }
+    }
+}
---
+++ Infrastructure/EntityFramework/Configuration/ProdutoConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.EntityFramework.Configuration
+{
+    internal class ProdutoConfiguration : IEntityTypeConfiguration<Produto>
+    {
+        public void Configure(EntityTypeBuilder<Produto> builder)
+        {
+            builder.HasKey(p => p.Id);
+            builder.Property(p => p.Id).ValueGeneratedOnAdd().UseIdentityColumn();
+            builder.Property(p => p.Nome).IsRequired().HasMaxLength(50);
+        }
+    }
+}
