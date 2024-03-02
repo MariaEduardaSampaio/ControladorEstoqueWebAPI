353f8d7fbfa311553636167128392b94cd715e20
Maria Eduarda Sampaio <dudah.s3@gmail.com>
2024-03-01 23:36:24 -0300

    add: adiciona interfaces de serviços

---
+++ Domain/Interfaces/Services/ILoteService.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Entities;
+using Domain.Requests;
+
+namespace Domain.Interfaces.Services
+{
+    public interface ILoteService
+    {
+        Lote Adicionar(LoteRequest request);
+        IEnumerable<Lote> ListarTodosOsLotes();
+        IEnumerable<Lote> ListarLotesPorProduto(int idProduto);
+        IEnumerable<Lote> ListarLotesVencidos();
+        Lote ListarLotePorCodigo(string codigo);
+        Lote ListarLotePorID(int idLote);
+        Lote Atualizar(LoteRequest request);
+        Lote Remover(int idLote);
+        IEnumerable<Lote> RemoverLotesVencidos();
+    }
+}
---
+++ Domain/Interfaces/Services/IProdutoService.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Entities;
+using Domain.Requests;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Interfaces.Services
+{
+    public interface IProdutoService
+    {
+        Produto AdicionarProduto(ProdutoRequest request);
+        IEnumerable<Produto> ListarTodosOsProdutos();
+        IEnumerable<Produto> ListarProdutosPorNome(string nome);
+        Produto ListarProdutoPorID(int idProduto);
+        Produto Atualizar(ProdutoRequest request);
+        Produto Remover(int idProduto);
+    }
+}
