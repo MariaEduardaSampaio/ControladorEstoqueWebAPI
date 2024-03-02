74efff4181b149ab68294012f7aac683bfd96a3c
Maria Eduarda Sampaio <dudah.s3@gmail.com>
2024-03-01 23:41:20 -0300

    add: adiciona requests

---
+++ Domain/Requests/LoteRequest.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Domain.Requests
+{
+    public class LoteRequest
+    {
+        [Required]
+        public string Codigo { get; set; }
+        [Required]
+        public DateOnly Validade { get; set; }
+        [Required]
+        public int UnidadesProdutos { get; set; }
+        [Required]
+        public int ProdutoId { get; set; }
+    }
+}
