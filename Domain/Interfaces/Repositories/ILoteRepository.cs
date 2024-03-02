89f8b487925286919a46ab0c9aa8a9005454af9f
Maria Eduarda Sampaio <dudah.s3@gmail.com>
2024-03-01 23:35:38 -0300

    adiciona nulabilidade de retornos get e novo método para deletar lotes vencidos

--- Domain/Interfaces/Repositories/ILoteRepository.cs
+++ Domain/Interfaces/Repositories/ILoteRepository.cs
@@ -5,11 +5,13 @@ namespace Domain.Interfaces.Repositories
     public interface ILoteRepository
     {
         int Create(Lote lote);
-        IEnumerable<Lote> GetAll();
-        IEnumerable<Lote> GetAllByProduct(int idProduct);
+        IEnumerable<Lote>? GetAll();
+        IEnumerable<Lote>? GetAllByProduct(int idProduct);
         Lote? GetByID(int id);
         Lote? GetByCode(string code);
         void Update(Lote lote);
         Lote? Delete(int id);
+        IEnumerable<Lote>? DeleteExpiredBatches();
+
     }
 }
