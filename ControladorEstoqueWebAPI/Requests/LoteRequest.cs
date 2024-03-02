9a8e139af1728836fb104a983cb4155517c2d3ff
Maria Eduarda Sampaio <dudah.s3@gmail.com>
2024-03-01 23:41:02 -0300

    add: adiciona classes requests

---
+++ ControladorEstoqueWebAPI/Requests/LoteRequest.cs
@@ -0,0 +1,12 @@
+﻿namespace Domain.Requests
+{
+    public class LoteRequest
+    {
+        //Lote Adicionar(LoteRequest request);
+        //Lote? Remover(int idLote);
+        //IEnumerable<Lote> ListarLotes();
+        //Lote? ListarLotePorCodigo(string codigo);
+        //Lote? ListarLotePorID(int idLote);
+        //Lote? Atualizar(LoteRequest request);
+    }
+}
